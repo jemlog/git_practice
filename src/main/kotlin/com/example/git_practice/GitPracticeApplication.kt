c0ed462043a63b481952f75f4b2f4903da2698d6
jemin <jemin03120111@gmail.com>
2023-09-25 19:02:03 +0900

    initial commit

---
+++ src/main/kotlin/com/example/git_practice/GitPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.git_practice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GitPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<GitPracticeApplication>(*args)
+}
